fbc5669abb26339d35fd200fa4d9de3287c75f13
vengadesh-15127 <vkvengadesh2@gmail.com>
2022-12-28 12:49:49 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count,setCount] = useState(1);
+  const [winner, setWinner] = useState('');
+  const [char, setChar] = useState('X');
+  const [matrix, setMatrix] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
+
+  const getBGClass = (value) => {
+    if (value === 'X') return 'yellow';
+    if (value === 'O') return 'orange';
+    return ''
+  }
+
+  const handleClick = (r, c) => {
+    if (matrix[r][c]) return;
+    const tempMatrix = [...matrix]
+    tempMatrix[r][c] = char;
+    setMatrix(tempMatrix);
+    setChar(char === 'X' ? 'O' : 'X');
+    setCount(count+1)
+    checkWinner();
+  }
+
+  const checkWinner = () => {
+    //Check row
+    if (
+      matrix[0][0] && 
+      matrix[0][0] === matrix[0][1] && 
+      matrix[0][1] === matrix[0][2]) {
+      setWinner('Player ' + matrix[0][0] + ' is a winner')
+    }
+    if (matrix[1][0] && 
+      matrix[1][0] === matrix[1][1] && 
+      matrix[1][1] === matrix[1][2]) {
+      setWinner('Player ' + matrix[1][0] + ' is a winner')
+    }
+    if (matrix[2][0]  && 
+      matrix[2][0] === matrix[2][1] && 
+      matrix[2][1] === matrix[2][2]) {
+      setWinner('Player ' + matrix[2][0] + ' is a winner')
+    }
+
+    // Check column
+    if (matrix[0][0] && 
+      matrix[0][0] === matrix[1][0] && 
+      matrix[1][0] === matrix[2][0]) {
+      setWinner('Player ' + matrix[0][0] + ' is a winner')
+    }
+    if (matrix[0][1] && 
+      matrix[0][1] === matrix[1][1] && 
+      matrix[1][1] === matrix[2][1]) {
+      setWinner('Player ' + matrix[0][1] + ' is a winner')
+    }
+    if (matrix[0][2] && 
+      matrix[0][2] === matrix[1][2] && 
+      matrix[2][2]) {
+      setWinner('Player ' + matrix[0][2] + ' is a winner')
+    }
+
+    //Check Diagonal
+    if (matrix[0][0] && 
+      matrix[0][0] === matrix[1][1] && 
+      matrix[1][1] === matrix[2][2]) {
+      setWinner('Player ' + matrix[0][0] + ' is a winner')
+    }
+    if (matrix[0][2] && 
+      matrix[0][2] === matrix[1][1] && 
+      matrix[1][1] === matrix[2][0]) {
+      setWinner('Player ' + matrix[0][2] + ' is a winner')
+    }
+    if(count===9){
+      setWinner("Match has been drawn");
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className='header alignCenter ' >Tic Tac Toe</div>
+      <div className='alignCenter board' >
+        {!winner && <p>{char} turn now</p>}
+        <div className='gameBoard' >
+          {winner || 
+            matrix.map((row, rIndex) => (
+              <div className='row'>
+                {
+                  row.map((cell, cIndex) => (
+                    <div
+                      onClick={() => handleClick(rIndex, cIndex)}
+                      className={`cell alignCenter ${getBGClass(matrix[rIndex][cIndex])}`
+                      }>
+                      {matrix[rIndex][cIndex]}
+                    </div>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
+        <button onClick={() => {
+          setWinner('');
+          setMatrix([
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', ''],
+          ])
+          setChar('X')
+        }}>Restart Game</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
